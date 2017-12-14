7341c25103ce69c06a03fe78edf37a48d2797947
paarthlakhani <paarth.lakhani@utah.edu>
2017-12-13 18:31:54 -0800

    Basic User Tutorial has been created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :about, :about_team, :user_tutorial]
+  skip_before_action :authenticate_user!, only: [:index, :about, :about_team, :user_tutorial, :tutorial_profile, :tutorial_groups_pets, :tutorial_lost_and_found, :tutorial_event_invitations, :tutorial_sign_up, :tutorial_group_chats, :tutorial_event_basics, :tutorial_ios_sign_in, :tutorial_ios_groups, :tutorial_ios_feeding, :tutorial_ios_nudge_group, :tutorial_ios_aroundme]
 
   # about the project
   def about
@@ -7,6 +7,42 @@ class HomeController < ApplicationController
   end
 
   def user_tutorial
+  end
+
+  def tutorial_profile
+  end
+
+  def tutorial_groups_pets
+  end
+
+  def tutorial_lost_and_found
+  end
+
+  def tutorial_event_invitations
+  end
+
+  def tutorial_sign_up
+  end
+
+  def tutorial_group_chats
+  end
+  
+  def tutorial_event_basics
+  end
+
+  def tutorial_ios_sign_in
+  end
+
+  def tutorial_ios_groups
+  end
+
+  def tutorial_ios_feeding
+  end
+
+  def tutorial_ios_nudge_group
+  end
+
+  def tutorial_ios_aroundme
   end
 
   def about_team
